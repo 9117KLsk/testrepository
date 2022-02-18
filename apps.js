13fd54aa146722e644cef1f18f3e05ee2010b6e8
Marcelo <luiggimanterola@hotmail.com>
2022-02-18 10:18:14 -0700

    apps.js

--- apps.js
+++ apps.js
@@ -1,32 +1,40 @@
-let id = Symbol("id");
-let id2 = Symbol("id2");
-console.log(id===id2);
-console.log(id,id2);
-console.log(typeof id, typeof id2);
+const set = new Set([11,2,3,3,4,5,true,false,true,'Hola','HOla']);
+console.log(set);
+console.log(set.size);  // en Set se usa size en lugar de length
 
-const NOMBRE = Symbol('Nombre');
-const SALUDAR = Symbol('Saludar');
-const persona = {
-[NOMBRE]:"Jon",
-edad:35
+const set2 = new Set();
+set2.add(1);
+set2.add(2);
+set2.add(2);
+set2.add(3);
+set2.add(true);
+set2.add(false);
+set2.add(true);
+set2.add({});
+ 
+console.log(set2);
+console.log(set2.size);
+
+console.log('Recorriendo set');
+
+for (item of set) {
+    console.log(item);
 }
-console.log(persona);
 
-persona.NOMBRE = "Jonathan Mircha";
-console.log(persona);
+console.log('Recorriendo set2');
 
-console.log(persona.NOMBRE);
-console.log(persona[NOMBRE]);
+set2.forEach(item=>console.log(item));
+console.log({});
 
-persona[SALUDAR] = function() {
-    console.log('Hola');
-}
+let arr = Array.from(set);  // convierte en arreglo el objeto Set
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[8]);
 
-console.log(persona);
-persona[SALUDAR]();
+set.delete('HOla');
+console.log(set);
+console.log(set.has('hola'));
+console.log(set.has(19));
 
-for (let propiedad in persona) {
-    console.log(propiedad);
-    console.log(persona[propiedad]);
-}
-console.log(Object.getOwnPropertySymbols(persona));
+set2.clear();
+console.log(set2)
