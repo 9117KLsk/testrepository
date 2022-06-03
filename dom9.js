d07073915407c210d4ad08296dda24fcc98c29a9
Marcelo <marcelodob@hotmail.com usuario colaborador nuevo>
2022-06-03 08:30:24 -0700

    DOM. Modificando elementos (Cool Style)

---
+++ dom9.js
@@ -0,0 +1,31 @@
+/*
+.insertAdjacent...
+  .insertAdjacentElement(position, el)
+  .insertAjacentHTML(position, html)
+  .insertAdjacentText(position, text)
+
+
+Posiciones:
+  beforebegin(hermano anterior)
+  afterbegin(primer hijo)
+  beforeend(ultimo hijo)
+  afterend(hermano siguiente)
+*/
+
+const $cards = document.querySelector(".cards"),
+$newCard = document.createElement("figure");
+
+let $contenCard = `
+    <img src = "https://placeimg.com/200/200/any" alt="Any" > 
+    <figcaption></figcaption>
+`;
+$newCard.classList.add("card");
+
+$newCard.insertAdjacentHTML("beforeend", $contenCard);
+$newCard.querySelector("figcaption").insertAdjacentText("afterbegin","Any");
+//$cards.insertAdjacentElement("afterbegin", $newCard);
+
+//$cards.prepend($newCard);
+//$cards.append($newCard);
+//$cards.before($newCard);
+//$cards.after($newCard);
