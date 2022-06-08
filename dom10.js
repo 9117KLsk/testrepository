989c2ab0e7251eccb8babc765410b9a7ab1044ae
Marcelo <marcelodob@hotmail.com usuario colaborador nuevo>
2022-06-08 10:26:40 -0700

    no funcionaron los manejadores de eventos semánticos ni múltiples

---
+++ dom10.js
@@ -0,0 +1,23 @@
+function holaMundo() {
+    alert("Hola Mundo");
+    console.log(event);
+}
+
+const $eventoSemantico = document.getElementById("evento-semantico"),
+$eventoMultiple = document.getElementById("evento-multiple");
+$eventoSemantico.onclick = holaMundo;
+$eventoSemantico.onclick = function(e) {
+  alert('Hola Mundo Manejador de Eventos Semántico');
+  console.log(e);
+  console.log(event);
+}
+
+$eventoMultiple.addEventListener("click", holaMundo);
+$eventoMultiple.addEventListener("click", (e) => {
+    alert('Hola Mundo Manejador de Eventos Semántico');
+    console.log(e);
+    console.log(e.type);
+    console.log(e.target);
+    console.log(event);
+});
+
